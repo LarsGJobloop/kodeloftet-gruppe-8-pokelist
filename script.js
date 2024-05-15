628fd508a0249a5299f881d5177e91f79ac8695f
LarsGJobloop <lars.gunnar@jobloop.no>
2024-05-15 10:40:59 +0200

    add pokemon data

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello!');
+import { pokemonList } from './pokemonData.js'
+
+// How to access individual fields of objects
+// const lars = {
+//     name: "Lars",
+//     age: 34,
+// }
+// console.log(lars)
+// console.log(lars.name)
+// console.log(lars.age)
+
+console.log(pokemonList.results)
