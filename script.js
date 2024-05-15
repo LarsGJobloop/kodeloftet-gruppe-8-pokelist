19dd96f507d72f433ee355523f9274e49e84191b
LarsGJobloop <lars.gunnar@jobloop.no>
2024-05-15 11:35:22 +0200

    setup rendering of pokemon list

--- script.js
+++ script.js
@@ -9,4 +9,21 @@ import { pokemonList } from './pokemonData.js'
 // console.log(lars.name)
 // console.log(lars.age)
 
-console.log(pokemonList.results)
+// console.log(pokemonList.results)
+
+for (const pokemon of pokemonList.results) {
+    // Lage et html element
+    const newElement = createPokemonCard(pokemon)
+
+    // Sett inn i dokument
+    const list = document.getElementById("pokemon-list")
+    list.appendChild(newElement)
+}
+
+function createPokemonCard(pokemon) {
+    const pokemonCard = document.createElement("li")
+    pokemonCard.textContent = pokemon.name
+    pokemonCard.className = "pokemon-card"
+
+    return pokemonCard
+}
