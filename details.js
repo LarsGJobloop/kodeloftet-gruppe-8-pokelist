a22dcc0458244ba899b67a8a1d1c148a5f206944
LarsGJobloop <lars.gunnar@jobloop.no>
2024-05-15 12:03:49 +0200

    add fetching of data on details page

---
+++ details.js
@@ -0,0 +1,18 @@
+// Acquire the information
+const baseUrl = "https://pokeapi.co/api/v2/pokemon/"
+
+const queryParameters = window.location.search
+const pokemonName = queryParameters.split("=")[1]
+const url = baseUrl + pokemonName
+
+
+// How to send messages accross the network
+const response = await fetch(url)
+const data = await response.json()
+
+// Make use of the data
+console.log(data)
+const nameElement = document.createElement("h1")
+nameElement.textContent = data.name
+
+document.body.appendChild(nameElement)
